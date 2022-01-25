8aa6de003ec695cd58539b305b3b32d737b490b7
Itzuzume <ellson637@gmail.com>
2022-01-25 16:01:27 +0800

    Update 1
    created a script for timer and collect coins

---
+++ Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/TimerObject.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class TimerObject : MonoBehaviour
+{
+
+    private float scoreValue;
+
+    public float totalCoins;
+
+    public float timeLeft;
+
+    public int timeRemaining;
+
+    public Text ScoreText;
+    public Text TimerText;
+
+    private float TimerValue;
+
+    //public int theScore;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeLeft -= Time.deltaTime;
+
+        timeRemaining = Mathf.FloorToInt(timeLeft % 60);
+
+        TimerText.text = "Timer: " + timeRemaining.ToString();
+
+        if(scoreValue >= 10)
+        {
+            Debug.Log("Win");
+            /*if(timeLeft <= TimerValue)
+            {
+                Debug.Log("Win");
+                //SceneManager.LoadScene("");
+            }*/
+        }
+        else if (timeLeft <= 0)
+        {
+            Debug.Log("Lose");
+            //SceneManager.LoadScene("");
+        }
+
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Coins")
+        {
+            scoreValue += 10;
+            ScoreText.GetComponent<Text>().text = "Score: " + scoreValue;
+            Destroy(other.gameObject);
+        }
+        
+        if(other.gameObject.tag == "Water")
+        {
+            Debug.Log("Lose");
+            //SceneManager.LoadScene("");
+        }
+
+
+    }
+}
