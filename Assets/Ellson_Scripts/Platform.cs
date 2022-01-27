d9b9babc817b09ffca6f073b31db964226c6b2ec
Itzuzume <ellson637@gmail.com>
2022-01-27 18:09:04 +0800

    Update 9
    scrap idea of moving platform

---
+++ Assets/Ellson_Scripts/Platform.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platform : MonoBehaviour
+{
+    public GameObject Players;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if(other.gameObject == Players)
+        {
+            Debug.Log("on");
+            Players.transform.parent = transform;
+        }
+
+
+        
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.gameObject == Players)
+        {
+            Debug.Log("off");
+            Players.transform.parent = null;
+        }
+
+        
+    }
+}
